05235d3ab7389eb2e7c41fc63f5319596dc4ec41
Rhys Clarke <rhys@rogodigital.com>
2025-12-15 22:35:14 +0000

    Add list displaying dummy data

--- gigFinder/views.py
+++ gigFinder/views.py
@@ -1,6 +1,12 @@
+import json
+from pathlib import Path
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'gigFinder/main.html')
+    json_path = Path(__file__).parent / 'test-data.json'
+    with open(json_path, 'r') as f:
+        gigs = json.load(f)
+    
+    return render(request, 'gigFinder/main.html', {'gigs': gigs})
